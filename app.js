a2c6c5bd88fdf1e75eebbff53b9f061d94ed020d
AwadYMohammed <awadyasirawad@gmail.com>
2020-09-19 02:56:08 -0400

    implementing attack method

--- app.js
+++ app.js
@@ -12,5 +12,21 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
             
-	 }}
+	 }},
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+            if (this.checkWin()) {
+                return;
+            }
+
+            this.monsterAttacks();
+        },
+		calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
 });
